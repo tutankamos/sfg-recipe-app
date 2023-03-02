6cf7f28d1ebd6022d4ec8d2f5c75fe5fd5548a68
Amos Torretta <torrettaamos@gmail.com>
2023-03-02 18:09:21 +0100

    schifezza mockmvc

--- src/test/java/guru/springframework/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/controllers/IndexControllerTest.java
@@ -7,6 +7,9 @@ import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import java.util.HashSet;
@@ -31,6 +34,15 @@ public class IndexControllerTest {
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
         indexController = new IndexController(recipeService);
+    }
+
+    @Test
+    public void testMockMVC() {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(indexController).build(); // per costruire il mockMVC
+
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view)
     }
 
     @Test
